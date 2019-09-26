628f7a514f32ae5eb41b05b2e4fc70ba12b2e8e4
Todd McLeod <tuddleymc@gmail.com>
2019-09-26 15:17:44 -0700

    a second example of interfaces

--- main.go
+++ main.go
@@ -10,6 +10,15 @@ func (p person) speak() {
 	fmt.Println("from a person - this is my name", p.first)
 }
 
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (sa secretAgent) speak() {
+	fmt.Println("I'm a secret agent - this is my name", sa.first)
+}
+
 // any TYPE that has the methods specified by an interface
 // is also of the interface type
 // an interface says
@@ -21,14 +30,23 @@ type human interface {
 
 func main() {
 	p1 := person{
-		first: "James",
+		first: "Miss Moneypenny",
+	}
+
+	sa1 := secretAgent{
+		person: person{
+			first: "James",
+		},
+		ltk: true,
 	}
 
 	fmt.Printf("%T\n", p1)
 
 	// in go a VALUE can be of more than one TYPE
 	// in this example, p1 is both TYPE person and TYPE human
-	var x human
+	var x, y human
 	x = p1
-	fmt.Printf("%T\n", x)
+	y = sa1
+	x.speak()
+	y.speak()
 }
