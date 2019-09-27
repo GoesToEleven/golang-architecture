57eae49baddb867677aa2bfe50b8be40bdcb7b06
Daniel Hoffmann <oralordos@gmail.com>
2019-09-27 13:34:21 -0700

    Ninja 02 exercise 02

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/GoesToEleven/golang-architecture
 
 go 1.13
-
-require github.com/golang/mock v1.3.1
--- main.go
+++ main.go
@@ -1,26 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type hotdog int
-
-func (h hotdog) cook() {
-	fmt.Println("I am cooking")
+type user struct {
+	first string
 }
 
-type hotFood interface {
-	cook()
+type mongo map[int]user
+
+func (m mongo) save(n int, u user) {
+	m[n] = u
 }
 
-func bar(hf hotFood) {
-	hf.cook()
+func (m mongo) retrieve(n int) user {
+	return m[n]
+}
+
+type harddrive map[int]user
+
+func (hd harddrive) save(n int, u user) {
+	hd[n] = u
+}
+
+func (hd harddrive) retrieve(n int) user {
+	return hd[n]
+}
+
+type accessor interface {
+	save(n int, u user)
+	retrieve(n int) user
+}
+
+func put(a accessor, n int, u user) {
+	a.save(n, u)
+}
+
+func get(a accessor, n int) user {
+	return a.retrieve(n)
 }
 
 func main() {
-	var x hotdog = 42
-	var y hotFood
-	y = x
-	fmt.Printf("%T\n", y)
+	storage := harddrive{}
 
-	bar(x)
+	u1 := user{
+		first: "James",
+	}
+
+	u2 := user{
+		first: "Jenny",
+	}
+
+	put(storage, 1, u1)
+	put(storage, 2, u2)
+
+	fmt.Println(get(storage, 1))
+	fmt.Println(get(storage, 2))
 }
