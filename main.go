79c6aa30e04c8ac73b8ddb47437dfa97977c88ca
Daniel Hoffmann <oralordos@gmail.com>
2019-09-30 12:39:44 -0700

    Add context timeout example

--- main.go
+++ main.go
@@ -1,27 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"io"
-	"os"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
+func makeRequest(ctx context.Context, route string) (string, error) {
+	deadline, ok := ctx.Deadline()
+	if ok && time.Until(deadline) < 100*time.Millisecond {
+		return "", fmt.Errorf("Deadline too near")
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, route, nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("Bad status code: %d", resp.StatusCode)
+	}
+
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bs), nil
+}
+
 func main() {
-	f, err := os.Open("file-01.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+	defer cancel()
 
-	f2, err := os.Create("file-02.txt")
+	resp, err := makeRequest(ctx, "https://www.google.com")
 	if err != nil {
 		panic(err)
 	}
-	defer f2.Close()
 
-	n, err := io.Copy(f2, f)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("bytes written", n)
+	fmt.Println(resp)
 }
