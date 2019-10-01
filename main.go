b16113c131bd7ed0e6243ab150f5d8bd5b8d62bc
Daniel Hoffmann <oralordos@gmail.com>
2019-10-01 15:09:18 -0700

    Add custom error example

--- main.go
+++ main.go
@@ -3,17 +3,33 @@ package main
 import (
 	"errors"
 	"fmt"
+	"time"
 )
+
+type ErrFileNotFound struct {
+	Filename string
+	When     time.Time
+}
+
+func (e ErrFileNotFound) Error() string {
+	return fmt.Sprintf("File %s was not found at %v", e.Filename, e.When)
+}
+
+func (e ErrFileNotFound) Is(other error) bool {
+	_, ok := other.(ErrFileNotFound)
+	return ok
+}
 
 var ErrNotExist = fmt.Errorf("File does not exist")
 var ErrUserNotExist = errors.New("User does not exist")
 
 func main() {
-	err := ErrUserNotExist
-
-	if err == ErrUserNotExist {
-		fmt.Println("You need to register first!")
-	} else {
-		fmt.Println("Unknown error")
+	err := ErrFileNotFound{
+		Filename: "test.txt",
+		When:     time.Now(),
 	}
+
+	fmt.Println(errors.Is(err, ErrFileNotFound{}))
+
+	fmt.Println(err)
 }
