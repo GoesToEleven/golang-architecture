f48c6eabbb6740678d97567cd98532a2fe993226
Todd McLeod <tuddleymc@gmail.com>
2019-10-01 12:35:42 -0700

    ninja 03 ex 05 TRY NUMBER ONE

--- main.go
+++ main.go
@@ -3,21 +3,34 @@ package main
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"time"
 )
 
 func main() {
+	fmt.Printf("GOURTINES RUNNING %d\n", runtime.NumGoroutine())
 	ctx := context.Background()
-	ctx, cancelF := context.WithTimeout(ctx, 500*time.Millisecond)
+	ctx, cancelF := context.WithCancel(ctx)
 	defer cancelF()
 
-	time.Sleep(100 * time.Millisecond)
-
-	select {
-	case <-ctx.Done():
-		fmt.Println("work didn't finish")
-	default:
-		fmt.Println("work finished")
+	for i := 0; i < 100; i++ {
+		go func(n int) {
+			fmt.Println("launching goroutine", n)
+			for {
+				select {
+				case <-ctx.Done():
+					runtime.Goexit()
+					// return
+				default:
+					fmt.Printf("goroutine %d is doing work\n", n)
+					time.Sleep(50 * time.Millisecond)
+				}
+			}
+		}(i)
 	}
-
+	time.Sleep(time.Millisecond)
+	fmt.Printf("GOURTINES RUNNING AFTER ONE MILLISECOND!!! %d\n", runtime.NumGoroutine())
+	cancelF()
+	time.Sleep(100 * time.Millisecond)
+	fmt.Printf("GOURTINES RUNNING AFTER CANCELFUNC CALLED %d\n", runtime.NumGoroutine())
 }
