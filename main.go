76ec8d7e95791796c39c03e1d2081a1e12548da1
Daniel Hoffmann <oralordos@gmail.com>
2019-10-02 12:09:57 -0700

    Add unwrap example

--- main.go
+++ main.go
@@ -1,48 +1,39 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
-	"os"
+	"errors"
 )
 
+func cat() error {
+	return errors.New("cat is an error")
+}
+
+func moo() error {
+	return fmt.Errorf("moo is an error: %w", cat())
+}
+
+func bar() error {
+	return fmt.Errorf("bar is an error: %w", moo())
+}
+
+func foo() error {
+	return fmt.Errorf("foo is an error: %w", bar())
+}
+
 func main() {
-	f, err := os.Open("rumi.txt")
+	err := foo()
+	fmt.Println(err)
 
-	var perr *os.PathError
+	baseErr := errors.Unwrap(err)
+	fmt.Println(baseErr)
 
-	switch {
-	case errors.Is(err, os.ErrPermission) && errors.As(err, &perr):
-		err = fmt.Errorf("you do not have permission to open the file: %w and the path is %s", err, perr.Path)
-		log.Println(err)
-	case errors.Is(err, os.ErrNotExist) && errors.As(err, &perr):
-		err = fmt.Errorf("the file does not exist: %w and the path is %s", err, perr.Path)
-		log.Println(err)
-	case errors.As(err, &perr):
-		err = fmt.Errorf("here is the orignial error %w and the path %s", err, perr.Path)
-		log.Println(err)
-	case err != nil:
-		err = fmt.Errorf("file couldn't be opened: %w", err)
-		log.Println(err)
-	}
+	baseErr = errors.Unwrap(baseErr)
+	fmt.Println(baseErr)
 
-	/*
-		if errors.Is(err, os.ErrPermission) && errors.As(err, &perr) {
-			err = fmt.Errorf("you do not have permission to open the file: %w and the path is %s", err, perr.Path)
-			log.Println(err)
-		} else if errors.Is(err, os.ErrNotExist) && errors.As(err, &perr) {
-			err = fmt.Errorf("the file does not exist: %w and the path is %s", err, perr.Path)
-			log.Println(err)
-		} else if errors.As(err, &perr) {
-			// return fmt.Errorf("here is the original error %w and the path %s", err, perr.Path) // RETURNS A ERROR
-			err = fmt.Errorf("here is the orignial error %w and the path %s", err, perr.Path)
-			log.Println(err)
-		} else if err != nil {
-			err = fmt.Errorf("file couldn't be opened: %w", err)
-			log.Println(err)
-		}
-	*/
+	baseErr = errors.Unwrap(baseErr)
+	fmt.Println(baseErr)
 
-	defer f.Close()
+	baseErr = errors.Unwrap(baseErr)
+	fmt.Println(baseErr)
 }
