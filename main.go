fcef672018a1c01c508aaf1c5d5777ae840de0d1
Todd McLeod <tuddleymc@gmail.com>
2019-10-02 11:10:04 -0700

    adds AS error checking

--- main.go
+++ main.go
@@ -10,29 +10,20 @@ import (
 func main() {
 	f, err := os.Open("rumi.txt")
 
-	if errors.Is(err, os.ErrPermission) {
-		err = fmt.Errorf("you do not have permission to open the file: %w", err)
+	var perr *os.PathError
+	if errors.Is(err, os.ErrPermission) && errors.As(err, &perr) {
+		err = fmt.Errorf("you do not have permission to open the file: %w and the path is %s", err, perr.Path)
 		log.Println(err)
-	} else if errors.Is(err, os.ErrNotExist) {
-		err = fmt.Errorf("the file does not exist: %w", err)
+	} else if errors.Is(err, os.ErrNotExist) && errors.As(err, &perr) {
+		err = fmt.Errorf("the file does not exist: %w and the path is %s", err, perr.Path)
+		log.Println(err)
+	} else if errors.As(err, &perr) {
+		// return fmt.Errorf("here is the original error %w and the path %s", err, perr.Path) // RETURNS A ERROR
+		err = fmt.Errorf("here is the orignial error %w and the path %s", err, perr.Path)
 		log.Println(err)
 	} else if err != nil {
 		err = fmt.Errorf("file couldn't be opened: %w", err)
 		log.Println(err)
 	}
-/*
-	if err == os.ErrPermission {
-		err = fmt.Errorf("you do not have permission to open the file: %w", err)
-		log.Println(err)
-	} else if err == os.ErrNotExist {
-		err = fmt.Errorf("the file does not exist: %w", err)
-		log.Println(err)
-	} else if err != nil {
-		// panic(err)
-
-		err = fmt.Errorf("file couldn't be opened: %w", err)
-		log.Println(err)
-	}
-*/
 	defer f.Close()
 }
