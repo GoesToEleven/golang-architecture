2eab015a6171d8ee02de8829fde823dcf8d63a95
Todd McLeod <tuddleymc@gmail.com>
2019-10-03 15:58:26 -0700

    stringer interface

--- main.go
+++ main.go
@@ -1,36 +1,37 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
+
+type person struct {
+	first string
+}
+
+func (p person) String() string {
+	return fmt.Sprintf("My name is %s", p.first)
+}
 
 func main() {
-	fooErr := foo()
-	barErr := errors.Unwrap(fooErr)
-	mooErr := errors.Unwrap(barErr)
-	catErr := errors.Unwrap(mooErr)
-	baseErr := errors.Unwrap(catErr)
-	fmt.Printf("fooErr\t%s\n", fooErr)
-	fmt.Printf("barErr\t%s\n", barErr)
-	fmt.Printf("mooErr\t%s\n", mooErr)
-	fmt.Printf("catErr\t%s\n", catErr)
-	fmt.Printf("baseErr\t%s\n", baseErr)
+	p1 := person{
+		first: "Jenny",
+	}
 
+	fmt.Println(p1)
+
+	// go's example
+	a := Animal{
+		Name: "Gopher",
+		Age:  2,
+	}
+	fmt.Println(a)
 }
 
-func foo() error {
-	return fmt.Errorf("this error is from FOO - %w", bar())
+// Animal has a Name and an Age to represent an animal.
+type Animal struct {
+	Name string
+	Age  uint
 }
 
-func bar() error {
-	return fmt.Errorf("this error is from BAR - %w", moo())
-}
-
-func moo() error {
-	return fmt.Errorf("this error is from MOO - %w", cat())
-}
-
-func cat() error {
-	return errors.New("this error is from CAT")
+// String makes Animal satisfy the Stringer interface.
+func (a Animal) String() string {
+	return fmt.Sprintf("%v (%d)", a.Name, a.Age)
 }
