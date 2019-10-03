84014430a5cfe518ac13fc22d56bd5c03f1f2496
Daniel Hoffmann <oralordos@gmail.com>
2019-10-03 16:23:59 -0700

    Heap escape example

--- main.go
+++ main.go
@@ -1,31 +1,18 @@
 package main
 
-import "fmt"
-
-type person struct {
-	first string
+//go:noinline
+func foo() int {
+	x := 42
+	return x
 }
 
-func (p person) String() string {
-	return fmt.Sprintf("My name is %s", p.first)
+//go:noinline
+func bar() *int {
+	y := 43
+	return &y
 }
 
 func main() {
-	p1 := person{
-		first: "Jenny",
-	}
-
-	p2 := person{
-		first: "James",
-	}
-
-	m := map[person]string{}
-
-	m[p1] = "loves james bond"
-	m[p2] = "loves james bond also"
-
-	fmt.Println(m[p1])
-	fmt.Println(m[p2])
-	fmt.Println(p1==p2)
-	fmt.Println(p1==p1)
+	_ = foo()
+	_ = bar()
 }
